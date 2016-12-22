930d12852274237e6acab10873eced045ec8f0a1
seojinchooo <seojinchooo@gmail.com>
2016-12-23 03:38:32 +0900

    Update AddressTest.java

--- AddressTest.java
+++ AddressTest.java
@@ -14,9 +14,7 @@ public void whenPasswordNotProperLengthVerificationTest() {
 	assertTrue(value.whetherIfPasswordProperLength("1462").equals("6~12자 내로 입력해주세요."));
 	assertTrue(value.whetherIfPasswordProperLength("1234567891012").equals("6~12자 내로 입력해주세요."));
 	assertTrue(value.whetherIfPasswordProperLength("ناموويكي شجرة المعرف الت تنمونها ").equals("6~12자 내로 입력해주세요."));
-	assertTrue(value.whetherIfPasswordProperLength("ن字儒·𡨸儒·𡦂儒 𡦂儒𡦂儒").equals("6~12자 내로 입력해주세요."));
-	
-	
+	assertTrue(value.whetherIfPasswordProperLength("ن字儒·𡨸儒·𡦂儒 𡦂儒𡦂儒").equals("6~12자 내로 입력해주세요."));		
 }
 
 @Test
@@ -31,7 +29,6 @@ public void whenPasswordNotMatchVerificationTest() {
 	Address value = new Address();
 	assertTrue(value.whetherIfPasswordMatch("شجرة المعر", "s分數2ر5分數jin").equals("비밀번호 확인이 잘못되었습니다."));
 	assertTrue(value.whetherIfPasswordMatch("s25jin", "-1-32-2-3/3").equals("비밀번호 확인이 잘못되었습니다."));
-	assertTrue(value.whetherIfPasswordMatch("dochanji", "dochanjin").equals("비밀번호 확인이 잘못되었습니다."));
-	
-}
+	assertTrue(value.whetherIfPasswordMatch("dochanji", "dochanjin").equals("비밀번호 확인이 잘못되었습니다."));	
+	}
 }
