b1e0077c10f03b9c64402ee830ce2244b11034d5
seojinchooo <seojinchooo@gmail.com>
2016-12-23 03:20:52 +0900

    code modified

--- Address.java
+++ Address.java
@@ -1,7 +1,7 @@
 
 public class Address {
 	
-	String whenNotContains010(String formPhone) {
+	String whetherIfContains010(String formPhone) {
 		if (!formPhone.contains("010"))
 			return "잘못된 값입니다.";
 		else
@@ -9,7 +9,7 @@ public class Address {
 		
 	}
 	
-	String whenNotProperLength(String formPass) {
+	String whetherIfPasswordProperLength(String formPass) {
 		if (formPass.length() < 6 || formPass.length() > 12)
 			return "6~12자 내로 입력해주세요.";
 		else
@@ -17,7 +17,7 @@ public class Address {
 		
 	}
 	
-	String whenPasswordNotMatch(String formPass, String formPassCheck) {
+	String whetherIfPasswordMatch(String formPass, String formPassCheck) {
 		if (!formPass.equals(formPassCheck))
 			return "비밀번호 확인이 잘못되었습니다.";
 		else
--- AddressTest.java
+++ AddressTest.java
@@ -2,33 +2,49 @@ import org.junit.*;
 import static org.junit.Assert.*;
 public class AddressTest {
 @Test
-public void whenNotContains010Test() {
-	Address a = new Address();
-	assertTrue(a.whenNotContains010("01112339399").equals("잘못된 값입니다."));
-	assertTrue(a.whenNotContains010("4569").equals("잘못된 값입니다."));
-	assertTrue(a.whenNotContains010("전화번호").equals("잘못된 값입니다."));
-	assertTrue(a.whenNotContains010("01099992222").equals("정상적으로 추가되었습니다."));
-	assertFalse(a.whenNotContains010("01099992222").equals("잘못된 값입니다."));
+public void whenContains010ValidationTest() {
+	Address value = new Address();
+	assertTrue(value.whetherIfContains010("01099992222").equals("정상적으로 추가되었습니다."));
+	assertTrue(value.whetherIfContains010("01071114655").equals("정상적으로 추가되었습니다."));
 }
-
 @Test
-public void whenNotProperLengthTest() {
-	Address a = new Address();
-	assertTrue(a.whenNotProperLength("가나다라마").equals("6~12자 내로 입력해주세요."));
-	assertTrue(a.whenNotProperLength("가나다라마바사아자차카타파").equals("6~12자 내로 입력해주세요."));
-	assertTrue(a.whenNotProperLength("abcde").equals("6~12자 내로 입력해주세요."));
-	assertTrue(a.whenNotProperLength("abcdefghijklm").equals("6~12자 내로 입력해주세요."));
-	assertTrue(a.whenNotProperLength("123456").equals("정상적으로 추가되었습니다."));
-	assertFalse(a.whenNotProperLength("123456").equals("6~12자 내로 입력해주세요."));
+public void whenNotContains010VerificationTest() {
+	Address value = new Address();
+	assertTrue(value.whetherIfContains010("01118881462").equals("잘못된 값입니다."));
+	assertTrue(value.whetherIfContains010("전화번호").equals("잘못된 값입니다."));
+	assertTrue(value.whetherIfContains010("-^&*^ㅉ*&^").equals("잘못된 값입니다."));
 	
 }
 
-public void whenPasswordNotMatchTest() {
-	Address a = new Address();
-	assertTrue(a.whenPasswordNotMatch("가나다", "가나다라").equals("비밀번호 확인이 잘못되었습니다."));
-	assertTrue(a.whenPasswordNotMatch("acd", "vds").equals("비밀번호 확인이 잘못되었습니다."));
-	assertFalse(a.whenPasswordNotMatch("가나다", "가나다").equals("비밀번호 확인이 잘못되었습니다."));
-	assertTrue(a.whenPasswordNotMatch("가나다", "가나다").equals("정상적으로 추가되었습니다."));
-	assertFalse(a.whenPasswordNotMatch("abcde", "abcde").equals("비밀번호 확인이 잘못되었습니다."));
+@Test
+public void whenPasswordProperLengthValidationTest() {
+	Address value = new Address();
+	assertTrue(value.whetherIfPasswordProperLength("s25jinch555").equals("정상적으로 추가되었습니다."));
+	assertTrue(value.whetherIfPasswordProperLength("gr22nt2@").equals("정상적으로 추가되었습니다."));
+	
+}
+
+@Test
+public void whenPasswordNotProperLengthVerificationTest() {
+	Address value = new Address();
+	assertTrue(value.whetherIfPasswordProperLength("1462").equals("6~12자 내로 입력해주세요."));
+	assertTrue(value.whetherIfPasswordProperLength("1234567891012").equals("6~12자 내로 입력해주세요."));
+	assertTrue(value.whetherIfPasswordProperLength("ناموويكي شجرة المعرف الت تنمونها ").equals("6~12자 내로 입력해주세요."));
+	assertTrue(value.whetherIfPasswordProperLength("ن字儒·𡨸儒·𡦂儒 𡦂儒𡦂儒").equals("6~12자 내로 입력해주세요."));
+	
+	
+}
+public void whenPasswordMatchValidationTest() {
+	Address value = new Address();
+	assertTrue(value.whetherIfPasswordMatch("s25jin", "s25jin").equals("정상적으로 추가되었습니다."));
+	assertTrue(value.whetherIfPasswordMatch("dochanjin", "dochanjin").equals("정상적으로 추가되었습니다."));
+}
+
+public void whenPasswordNotMatchVerificationTest() {
+	Address value = new Address();
+	assertTrue(value.whetherIfPasswordMatch("شجرة المعر", "s分數2ر5分數jin").equals("비밀번호 확인이 잘못되었습니다."));
+	assertTrue(value.whetherIfPasswordMatch("s25jin", "-1-32-2-3/3").equals("비밀번호 확인이 잘못되었습니다."));
+	assertTrue(value.whetherIfPasswordMatch("dochanji", "dochanjin").equals("비밀번호 확인이 잘못되었습니다."));
+	
 }
 }
