51887803da034b6ebad25d2a6483c03996b955de
seojinchooo <seojinchooo@gmail.com>
2016-12-23 09:58:17 +0900

    Update AddressTest.java

--- AddressTest.java
+++ AddressTest.java
@@ -1,34 +1,34 @@
-﻿import org.junit.*;
+import org.junit.*;
 import static org.junit.Assert.*;
+
 public class AddressTest {
 @Test
-public void whenPasswordProperLengthValidationTest() {
+public void whenNotEmptyValueValidationTest() {
 	Address value = new Address();
-	assertTrue(value.whetherIfPasswordProperLength("s25jinch555").equals("정상적으로 추가되었습니다."));
-	assertTrue(value.whetherIfPasswordProperLength("gr22nt2@").equals("정상적으로 추가되었습니다."));	
-}
+	assertTrue(value.whetherIfEmpty("홍길동", "01092993892").equals("정상적으로 추가되었습니다."));
+	assertTrue(value.whetherIfEmpty("김영희", "01029392233").equals("정상적으로 추가되었습니다."));	
+	}
 
 @Test
-public void whenPasswordNotProperLengthVerificationTest() {
+public void whenEmptyValueVerificationTest() {
 	Address value = new Address();
-	assertTrue(value.whetherIfPasswordProperLength("1462").equals("6~12자 내로 입력해주세요."));
-	assertTrue(value.whetherIfPasswordProperLength("1234567891012").equals("6~12자 내로 입력해주세요."));
-	assertTrue(value.whetherIfPasswordProperLength("ناموويكي شجرة المعرف الت تنمونها ").equals("6~12자 내로 입력해주세요."));
-	assertTrue(value.whetherIfPasswordProperLength("ن字儒·𡨸儒·𡦂儒 𡦂儒𡦂儒").equals("6~12자 내로 입력해주세요."));		
-}
+	assertTrue(value.whetherIfEmpty("", "01092993892").equals("값을 입력하세요."));
+	assertTrue(value.whetherIfEmpty("김영희", "").equals("값을 입력하세요."));	
+	assertTrue(value.whetherIfEmpty("", "").equals("값을 입력하세요."));	
+	}
 
 @Test
-public void whenPasswordMatchValidationTest() {
+public void whenOnlyNumberValueValidationTest() {
 	Address value = new Address();
-	assertTrue(value.whetherIfPasswordMatch("s25jin", "s25jin").equals("정상적으로 추가되었습니다."));
-	assertTrue(value.whetherIfPasswordMatch("dochanjin", "dochanjin").equals("정상적으로 추가되었습니다."));
-}
+	assertTrue(value.whetherItOnlyContainsNumber("01092993892").equals("정상적으로 추가되었습니다."));
+	assertTrue(value.whetherItOnlyContainsNumber("01029392233").equals("정상적으로 추가되었습니다."));	
+	}
 
 @Test
-public void whenPasswordNotMatchVerificationTest() {
+public void whenNotOnlyNumberValueVerificationTest() {
 	Address value = new Address();
-	assertTrue(value.whetherIfPasswordMatch("شجرة المعر", "s分數2ر5分數jin").equals("비밀번호 확인이 잘못되었습니다."));
-	assertTrue(value.whetherIfPasswordMatch("s25jin", "-1-32-2-3/3").equals("비밀번호 확인이 잘못되었습니다."));
-	assertTrue(value.whetherIfPasswordMatch("dochanji", "dochanjin").equals("비밀번호 확인이 잘못되었습니다."));	
+	assertTrue(value.whetherItOnlyContainsNumber("010dffs93892").equals("숫자만을 입력해주십시오."));
+	assertTrue(value.whetherItOnlyContainsNumber("^*&*").equals("숫자만을 입력해주십시오."));	
+	assertTrue(value.whetherItOnlyContainsNumber("abcd").equals("숫자만을 입력해주십시오."));	
 	}
 }
