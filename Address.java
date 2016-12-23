2ff9fe50a1b658bef0f4371686ed2c157d2f5f12
seojinchooo <seojinchooo@gmail.com>
2016-12-23 09:59:36 +0900

    Update Address.java

--- Address.java
+++ Address.java
@@ -1,15 +1,27 @@
 public class Address {
-	String whetherIfPasswordProperLength(String formPass) {
-		if (formPass.length() < 6 || formPass.length() > 12)
-			return "6~12자 내로 입력해주세요.";
+	String whetherIfEmpty(String formName, String formPhone) {
+		if ("".equals(formName) || "".equals(formPhone))
+			return "값을 입력하세요.";
 		else
 			return "정상적으로 추가되었습니다.";		
 	}
-	
-	String whetherIfPasswordMatch(String formPass, String formPassCheck) {
-		if (!formPass.equals(formPassCheck))
-			return "비밀번호 확인이 잘못되었습니다.";
+
+	String whetherItOnlyContainsNumber(String formPhone) {
+		if(!isNumber(formPhone))
+			return "숫자만을 입력해주십시오.";
 		else
-			return "정상적으로 추가되었습니다.";
-	}
+			return "정상적으로 추가되었습니다.";		
+	}	
+	 public static boolean isNumber(String str){
+	        boolean result = false; 
+	         
+	         
+	        try{
+	            Integer.parseInt(str) ;
+	            result = true ;
+	        }catch(Exception e){}
+	         
+	         
+	        return result ;
+	    }
 }
